48a76487a9a082150e7788ac405a42e2a80084e8
Ruoyu Qiu <cabbaken@outlook.com>
2025-04-03 01:55:36 +0800

    update
    Signed-off-by: Ruoyu Qiu <cabbaken@outlook.com>

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -23,12 +23,18 @@
 
 use std::num::ParseIntError;
 
-pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+pub fn total_cost(item_quantity: &str) -> Result<i32, String> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
-
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Err(ParseIntError) => {
+            return Err("invalid digit found in string".to_string())
+        },
+        Ok(value) => {
+            Ok(value * cost_per_item + processing_fee)
+        },
+    }
 }
 
 #[cfg(test)]
